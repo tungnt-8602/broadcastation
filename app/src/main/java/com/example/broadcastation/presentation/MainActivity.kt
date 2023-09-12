52729912b2165927e84db935d14d3d778531b599
tungnt-8602 <08062002thetung@gmail.com>
2023-09-12 17:04:18 +0700

    Build project structure

---
+++ app/src/main/java/com/example/broadcastation/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.broadcastation.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.broadcastation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
